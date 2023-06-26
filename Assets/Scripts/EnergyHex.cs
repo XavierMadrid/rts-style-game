0b1e2c03e6bbd8c2210e3949a0de9ac3709372e5
XavierMadrid <72964258+XavierMadrid@users.noreply.github.com>
2023-06-26 14:52:49 -0700

    made PowerHex inherit from the new EnergyClass script
    The EnergyHex script generalizes PowerHex to work with non powerHex energy hexes.

---
+++ Assets/Scripts/EnergyHex.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyHex : MonoBehaviour
+{
+    private List<GameObject> linkedHexes = new ();
+    
+    private const int linkRange = 3; // is multiplied by 6 to account for hex position intervals of 6
+    private int activatedHexesCount;
+    private bool hexActivated;
+
+    protected virtual bool HexActivated
+    {
+        get => hexActivated;
+        set
+        {
+            if (value != hexActivated)
+            {
+                HexActivation(value);
+                hexActivated = value;
+            }
+        }
+    }
+    
+    private Hex transformHexPos;
+    
+    protected virtual void Start()
+    {
+        transformHexPos = transform.position.ToHex();
+        
+        InitialSearchForLinks();
+    }
+
+
+    // Start is called before the first frame update
+    private void OnEnable()
+    {
+        ManagerReferences.Instance.HexBuilder.OnEnergyHexPlaced += EnergyHexPlaced;
+    }
+
+    protected virtual void EnergyHexPlaced(GameObject hexObject, Hex newHexPos)
+    {
+        activatedHexesCount = 0;
+        if (transformHexPos.DistanceTo(newHexPos) > linkRange * 6) return;
+        
+        for (int i = 0; i < linkedHexes.Count; i++)
+        {
+            if (linkedHexes[i] != null) continue;
+            
+            linkedHexes[i] = hexObject;
+
+            if (linkedHexes[i] == ManagerReferences.Instance.HexBuilder.CenterHex ||
+                linkedHexes[i].GetComponent<EnergyHex>().hexActivated) activatedHexesCount++;
+
+            break;
+        }
+
+        if (activatedHexesCount > 0) HexActivated = true;
+    }
+
+    private void InitialSearchForLinks()
+    {
+        activatedHexesCount = 0;
+        foreach (var keyValuePair in ManagerReferences.Instance.HexBuilder.EnergyHexPosDict)
+        {
+            if (transformHexPos.DistanceTo(keyValuePair.Key) > linkRange * 6 || transformHexPos.Equals(keyValuePair.Key)) continue; // is the hex in range
+
+            linkedHexes.Add(keyValuePair.Value);
+            
+            if (keyValuePair.Value.transform.position.ToHex().Equals(ManagerReferences.Instance.HexBuilder.CenterHex.transform.position.ToHex()) || 
+                keyValuePair.Value.GetComponent<EnergyHex>().HexActivated)
+            {
+                activatedHexesCount++;
+            }
+        }
+
+        HexActivated = activatedHexesCount > 0;
+    }
+
+    protected virtual void HexActivation(bool value)
+    {
+        foreach (var hexObject in linkedHexes)
+        {
+            if (hexObject.TryGetComponent<EnergyHex>(out var energyHex))
+            {
+                energyHex.HexActivated = true;
+            }
+        }
+    }
+
+    private void OnDisable()
+    {
+        ManagerReferences.Instance.HexBuilder.OnEnergyHexPlaced -= EnergyHexPlaced;
+    }
+}
--- Assets/Scripts/PowerHex.cs
+++ Assets/Scripts/PowerHex.cs
@@ -3,110 +3,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PowerHex : MonoBehaviour
+public class PowerHex : EnergyHex
 {
-    private List<GameObject> linkedHexes = new ();
     private SpriteRenderer sr;
     private readonly Color poweredColor = new(0.2972f, 0.7023f, 1, 1f);
     
-    private const int linkRange = 18;
-    private int activatedHexesCount;
-    private bool hexActivated;
-
-    private bool HexActivated
-    {
-        get => hexActivated;
-        set
-        {
-            if (value != hexActivated)
-            {
-                HexActivation(value);
-                hexActivated = value;
-            }
-        }
-    }
-    
-    private Hex transformHexPos;
-    
-    private void Start()
+    protected override void Start()
     {
         sr = GetComponent<SpriteRenderer>();
         sr.color = HexBuilder.POWER_HEX.HexColor;
         
-        transformHexPos = transform.position.ToHex();
-        
-        InitialSearchForLinks();
+        base.Start();
     }
 
-
-    // Start is called before the first frame update
-    private void OnEnable()
-    {
-        ManagerReferences.Instance.HexBuilder.OnEnergyHexPlaced += EnergyHexPlaced;
-    }
-
-    private void EnergyHexPlaced(GameObject hexObject, Hex newHexPos)
-    {
-        activatedHexesCount = 0;
-        if (transformHexPos.DistanceTo(newHexPos) > linkRange) return;
-        
-        for (int i = 0; i < linkedHexes.Count; i++)
-        {
-            if (linkedHexes[i] != null) continue;
-            
-            linkedHexes[i] = hexObject;
-
-            if (linkedHexes[i] == ManagerReferences.Instance.HexBuilder.CenterHex ||
-                linkedHexes[i].GetComponent<PowerHex>().hexActivated) activatedHexesCount++;
-
-            break;
-        }
-
-        if (activatedHexesCount > 0) HexActivated = true;
-    }
-
-    private void InitialSearchForLinks()
-    {
-        activatedHexesCount = 0;
-        foreach (var keyValuePair in ManagerReferences.Instance.HexBuilder.EnergyHexPosDict)
-        {
-            Debug.Log(keyValuePair.Key, keyValuePair.Value);
-            Debug.Log("transform: " + transformHexPos);
-            Debug.Log(keyValuePair.Value);
-            Debug.Log(keyValuePair.Value.transform.position.ToHex());
-            Debug.Log(transformHexPos.DistanceTo(keyValuePair.Key));
-            if (transformHexPos.DistanceTo(keyValuePair.Key) > linkRange || transformHexPos.Equals(keyValuePair.Key))
-            {
-                Debug.Log("failed");
-                continue; // is the hex in range
-            }
-
-            linkedHexes.Add(keyValuePair.Value);
-            
-            if (keyValuePair.Value.transform.position.ToHex().Equals(ManagerReferences.Instance.HexBuilder.CenterHex.transform.position.ToHex()) || 
-                keyValuePair.Value.GetComponent<PowerHex>().HexActivated)
-            {
-                Debug.Log("true");
-                activatedHexesCount++;
-            }
-        }
-
-        HexActivated = activatedHexesCount > 0;
-    }
-
-    private void HexActivation(bool value)
+    protected override void HexActivation(bool value)
     {
         StartCoroutine(value
             ? FadeToColor(HexBuilder.POWER_HEX.HexColor, poweredColor)
             : FadeToColor(poweredColor, HexBuilder.POWER_HEX.HexColor));
 
-        foreach (var hexObject in linkedHexes)
-        {
-            if (hexObject.TryGetComponent<PowerHex>(out var powerHex))
-            {
-                powerHex.HexActivated = true;
-            }
-        }
+        base.HexActivation(value);
     }
     
     private IEnumerator FadeToColor(Color startColor, Color targetColor)
@@ -121,10 +37,5 @@ public class PowerHex : MonoBehaviour
             yield return null;
         }
         sr.color = targetColor;
-    }
-    
-    private void OnDisable()
-    {
-        ManagerReferences.Instance.HexBuilder.OnEnergyHexPlaced -= EnergyHexPlaced;
     }
 }
